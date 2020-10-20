05ca9c65fc428b7dd82472e1d90980a480cb8835
Chris <mistercwood@gmail.com>
2020-10-20 13:00:16 +1000

    Completed reverseString exercise succcessfully.

--- reverseString/reverseString.js
+++ reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
-
+const reverseString = function(inputString) {
+        let input = inputString.split("");
+        let reversed = [];
+        while (input.length > 0) {
+            let x = input.pop();
+            reversed.push(x);
+        }
+        output = reversed.join("");
+        return output; 
 }
 
 module.exports = reverseString
