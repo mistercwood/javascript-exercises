ba79598d1ad345b52b4cb5b23f1357c4b4248819
Chris <mistercwood@gmail.com>
2020-10-20 12:15:31 +1000

    Completed RepeatString exercise successfuly

--- repeatString/repeatString.js
+++ repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function(string, number) {
+    if (number < 0) {
+        return "ERROR";
+    }
+    else {
+        let newString = ""; 
+        for (i = 0; i < number; i++) {
+            newString += string;
+        }
+        return newString;
+    }
 }
 
 module.exports = repeatString
