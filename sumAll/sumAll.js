21ddaee7956d8b8c5a49218886eda002bd3a02db
Woody <mistercwood@gmail.com>
2020-10-21 13:16:00 +1000

    Completed sumAll exercise successfully

--- sumAll/sumAll.js
+++ sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
-
+const sumAll = function(a,b) {
+    if ((typeof (a) != "number") || (typeof (b) != "number")) {
+        return "ERROR";
+    }
+    else if ((a < 0) || (b < 0)) {
+        return "ERROR";
+    }
+    else {
+    let large = (a > b) ? a : b;
+    let small = (a > b) ? b : a;
+    let total = 0;
+    for (let i = small; i < (large + 1); i++) {
+        total += i;
+    }
+    return total;
+    }
 }
 
 module.exports = sumAll
