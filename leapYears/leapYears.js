640ad1420e86ba921c2f8700954243640ab26216
Woody <mistercwood@gmail.com>
2020-10-21 14:59:20 +1000

    Completed leapYears exercise successfully.

--- leapYears/leapYears.js
+++ leapYears/leapYears.js
@@ -1,5 +1,9 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    let d4 = (year % 4) === 0;
+    let d100 = (year % 100) === 0;
+    let d400 = (year % 400) === 0;
+    let isLeap = (d100 && d400) || ((!d100) && d4);
+    return isLeap;
 }
 
 module.exports = leapYears
