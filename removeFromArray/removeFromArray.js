319b16e37caf35b2af33623d3b573209809f4a7d
Woody <mistercwood@gmail.com>
2020-10-21 12:00:12 +1000

    Completed removeFromArray exercise successfully.

--- removeFromArray/removeFromArray.js
+++ removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(...args) {
+    let arr = args[0];
+    let newArr = [];
+    arr.forEach((item) => {
+        if (!args.includes(item)) {
+        newArr.push(item);
+        }
+    });
+    return newArr;
 }
 
 module.exports = removeFromArray
